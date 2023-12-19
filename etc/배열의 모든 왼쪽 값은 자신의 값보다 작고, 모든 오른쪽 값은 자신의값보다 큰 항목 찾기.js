9700f5b26530004f913540321dea31ef776dd015
bm4656 <newjin46@gmail.com>
2023-12-20 03:54:49 +0900

    23-12-20

---
+++ etc/배열의 모든 왼쪽 값은 자신의 값보다 작고, 모든 오른쪽 값은 자신의값보다 큰 항목 찾기.js
@@ -0,0 +1,35 @@
+// 배열이 주어지고, 배열의 모든 왼쪽 값은 자신의 값 보다 작고, 모든 오른쪽 값은 자신의 값 보다 큰 항목을 찾는다.
+// 그런 조건에 맞는 항목을 찾으면 그 항목의 색인 값을 반환하고, 그런 항목이 없으면 -1 값을 반환한다. 예상 시간 복잡도는 O(n)
+
+function check(arr, n, index) {
+  let i = index - 1;
+  let j = index + 1;
+
+  while (i >= 0) {
+    if (arr[i] > arr[index]) {
+      return false;
+    }
+    i--;
+  }
+
+  while (j < n) {
+    if (arr[j] < arr[index]) {
+      return false;
+    }
+    j++;
+  }
+  return true;
+}
+
+function findElement(arr, n) {
+  for (let i = 1; i < n - 1; i++) {
+    if (check(arr, n, i)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+const arr = [5, 1, 4, 3, 6, 8, 10, 7, 9];
+const n = arr.length;
+console.log(findElement(arr, n));
--- solved.ac/algorithm/11659.js
+++ solved.ac/algorithm/11659.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/../input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
 const arr = input[1].split(' ').map(Number);
 const sumArr = new Array(arr.length + 1).fill(0);
 const solution = [];
