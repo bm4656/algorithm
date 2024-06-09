716ef1ae2795558782b6e4ed0f39de5494785fd4
Bomin <newjin46@gmail.com>
2024-06-09 23:45:38 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,19 @@
+function solution(progresses, speeds) {
+    let days=[];
+   let result ={};
+    for(let i =0; i<progresses.length; i++){
+        let remain = 100-progresses[i];
+        let day = Math.ceil(remain/speeds[i])
+        days.push(day)
+    }
+    // console.log(days)
+    for(let i=0; i<days.length-1; i++){
+        if(days[i]>days[i+1]){
+            days[i+1]=days[i];
+        }
+    }
+    days.forEach((x) => { 
+    result[x] = (result[x] || 0)+1; 
+    });
+    return Object.values(result);
+}
